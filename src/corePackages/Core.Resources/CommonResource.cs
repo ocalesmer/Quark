b98d7685d0463ecb133610905f3ff8b268b6aaf8
Öcal Esmer <ocal@avvamobile.com>
2022-08-25 01:00:22 +0300

    resources eklendi.

---
+++ src/corePackages/Core.Resources/CommonResource.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.Extensions.Localization;
+
+namespace Core.Resources
+{
+    public interface ICommonResource
+    {
+    }
+
+    public class CommonResource : ICommonResource
+    {
+        private readonly IStringLocalizer _localizer;
+
+        public CommonResource(IStringLocalizer<CommonResource> localizer)
+        {
+            _localizer = localizer;
+        }
+
+        public string this[string index]
+        {
+            get
+            {
+                return _localizer[index];
+            }
+        }
+    }
+}
